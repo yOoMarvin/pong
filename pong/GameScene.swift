eb9942a38afbd03ca1115b8cb5417aaea21d8833
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-02-09 14:10:29 +0100

    Setup elements in scene

--- pong/GameScene.swift
+++ pong/GameScene.swift
@@ -11,10 +11,25 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var ball = SKSpriteNode()
+    var enemy = SKSpriteNode()
+    var main = SKSpriteNode()
     
     override func didMove(to view: SKView) {
         
-            }
+        ball = self.childNode(withName: "ball") as! SKSpriteNode
+        enemy = self.childNode(withName: "enemy") as! SKSpriteNode
+        main = self.childNode(withName: "main") as! SKSpriteNode
+        
+        ball.physicsBody?.applyImpulse(CGVector(dx: 20, dy: 20))
+        
+        //border for scene
+        let border = SKPhysicsBody(edgeLoopFrom: self.frame)
+        border.friction = 0
+        border.restitution = 1
+        self.physicsBody = border
+        
+    }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
