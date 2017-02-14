192f32f948268831c2ea79ba7436e83007c67921
Marvin Messenzehl <marvin.messenzehl@gmail.com>
2017-02-14 13:52:26 +0100

    Added menu view and navigation

---
+++ MenuViewController.swift
@@ -0,0 +1,40 @@
+//
+//  MenuViewController.swift
+//  pong
+//
+//  Created by Marvin Messenzehl on 14.02.17.
+//  Copyright © 2017 Marvin Messenzehl. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MenuViewController: UIViewController {
+    
+    enum gameType {
+        case easy
+        case medium
+        case hard
+        case player2
+    }
+    
+    @IBAction func Player2(_ sender: Any) {
+        moveToGame(game: .player2)
+    }
+    
+    @IBAction func Easy(_ sender: Any) {
+        moveToGame(game: .easy)
+    }
+    @IBAction func Medium(_ sender: Any) {
+        moveToGame(game: .medium)
+    }
+    @IBAction func Hard(_ sender: Any) {
+        moveToGame(game: .hard)
+    }
+    
+    
+    func moveToGame(game: gameType){
+        let gameViewController = self.storyboard?.instantiateViewController(withIdentifier: "gameViewController") as! GameViewController
+        self.navigationController?.pushViewController(gameViewController, animated: true)
+    }
+}
